03e5dac616bd805085aec544841c59c3570e6075
Topinkaverdan <veronikadanilova439@gmail.com>
2023-03-26 22:30:45 +0300

    Changed file games.js

--- games.js
+++ games.js
@@ -15,3 +15,30 @@ function guessTheSeason() {
         alert('Попробуй снова! Такого месяца не существует:(');
     }
 }
+
+// second game 
+
+function guessTheWords() {
+let wordFruit = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин']
+wordFruit = wordFruit.sort(() => Math.random() - 0.5);
+
+alert(wordFruit);
+
+let firstUserAnswer = String(prompt('Чему равнялся первый элемент?'));
+let secondUserAnswer = String(prompt('Чему равнялся последний элемент?'));
+
+if (firstUserAnswer.toLowerCase() === wordFruit[0].toLowerCase() && secondUserAnswer.toLowerCase() === wordFruit[6].toLowerCase()) {
+
+    alert('Мои поздравления! Вы угадали оба элемент)');
+
+} else if (firstUserAnswer.toLowerCase() === wordFruit[0].toLowerCase() || secondUserAnswer.toLowerCase() === wordFruit[6].toLowerCase()) {
+    
+    alert('Вы были близки к победе!');
+
+} else {
+
+    alert('К сожалению, вы ответили неверно. Попробуйте снова(');
+
+}
+
+}
